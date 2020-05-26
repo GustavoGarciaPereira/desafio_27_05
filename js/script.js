61b78ae2d84ef6b4af8eaa72adf1560af1354f47
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-05-25 22:28:38 -0300

    versao final

--- js/script.js
+++ js/script.js
@@ -11,43 +11,40 @@ window.addEventListener("load", function(event) {
         console.error('Não foi possível achar a informação', err)
       })
     
-    console.log("gustavo") 
+
 })
 
 function busca1(lista){
     var busca = document.querySelector('#buscar')
     busca.addEventListener('click',()=>{
-        //lista.map((e)=>{
-        //    var nome = document.querySelector("#input_busca")
-        //    if(e.name.first.toUpperCase().includes(nome.value.toUpperCase())){
-        //        console.log("dd",e)
-        //        montar_lista_nomes(e)
-        //        montar_estatisticas(e)
-        //    }
-        //})
-        var nome = document.querySelector("#input_busca")
-        lista_dos_nomes = lista.filter((item)=>{
-            return item.name.first.toUpperCase().includes(nome.value.toUpperCase())            
-        });
-        montar_lista_nomes(lista_dos_nomes)
-        montar_estatisticas(lista_dos_nomes)
-        somas_idades(lista_dos_nomes)
-
-
+        center_dados()
     })
     document.addEventListener('keypress',(event)=>{
-        //lista.map((e)=>{
-        //    var nome = document.querySelector("#input_busca")
-        //    if(e.name.first.toUpperCase().includes(nome.value.toUpperCase())){
-        //        console.log(e,)
-        //    }
-        //})
-        if(event.charCode){}
         console.log(event.charCode)
+        if(event.charCode===13){
+            center_dados()
+        }
     })  
+
+    function center_dados(){
+        var nome = document.querySelector("#input_busca")
+        console.log("<>>>>>>>>>>",nome)
+        if (nome.value!==''){
+            lista_dos_nomes = lista.filter((item)=>{
+                return item.name.first.toUpperCase().includes(nome.value.toUpperCase())            
+            });
+            montar_lista_nomes(lista_dos_nomes)
+            montar_estatisticas(lista_dos_nomes)
+            calculo_media(lista_dos_nomes)
+        }else{
+            limpar()
+        }
+    }
+
 
     function montar_lista_nomes(lista){
         console.log(lista)
+        limpar()
         lista.map((lista)=>{
             document.querySelector("#nomes").innerHTML += `
             <li>
@@ -55,6 +52,7 @@ function busca1(lista){
            </li>
             `
         })
+        label_usuarios(lista.length)
  
     }
     function montar_estatisticas(lista){
@@ -65,16 +63,37 @@ function busca1(lista){
             return lista.gender==='female'
         })
 
-         
         document.querySelector("#sexo_masculino").innerHTML = homens.length
         document.querySelector("#sexo_feminino").innerHTML = mulheres.length
     }
 
     function somas_idades(lista){
+        const soma_idades = lista.reduce((accumulator, currentValue)=>{
+            return accumulator + (currentValue.dob.age)
+        },0)
+        console.log("teste",soma_idades)
+        document.querySelector("#soma_idades").innerHTML = soma_idades
 
+        return soma_idades
+    }
+    function calculo_media(lista){
+        let soma_idades = somas_idades(lista_dos_nomes)
+        document.querySelector("#medias_idades").innerHTML = ((soma_idades/lista.length).toFixed(2))
+        
+    }
+    function limpar(){
+        document.querySelector("#medias_idades").innerHTML = 0
+        document.querySelector("#soma_idades").innerHTML = 0
+        document.querySelector("#sexo_masculino").innerHTML = 0
+        document.querySelector("#sexo_feminino").innerHTML = 0
+        document.querySelector("#nomes").innerHTML = ""
+        label_usuarios(0)
 
-// 1 + 2 + 3 + 4
-        lista.reduce((accumulator, currentValue)=> accumulator + currentValue)
-// expected output: 10
+    }
+    function label_usuarios(numero){
+        document.querySelector("#usuarios").innerHTML = `
+            ${numero} usuáiros encontrados
+        `
+        
     }
 }
